0fdc13aab843d7c442dbe98f899cd323a0a460da
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-05-08 08:46:16 +0530

    basic folder structure

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Layout from './layout/Layout'
 function App() {
   
   return (
     <>
       <div>
-        <h1 className="text-primaryColor text-[20px] shadow-panelShadow
-        hover:text-tertiaryColor hover:underline sm:text-[30px] md:text-[50px]" >Hello World</h1>
+        <Layout />
         </div>
         
     </>
---
+++ frontEnd/src/routes/Routers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Routers = () => {
+  return (
+    <div>
+      <h1>Routers</h1>
+    </div>
+  )
+}
+
+export default Routers
