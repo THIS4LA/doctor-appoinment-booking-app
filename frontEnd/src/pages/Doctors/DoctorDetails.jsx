3f6a400fed35160df5cbd139dad9a8a2b6d58aa3
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-05-08 13:49:34 +0530

    create pages and routing those

---
+++ frontEnd/src/pages/Doctors/DoctorDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DoctorDetails = () => {
+  return (
+    <div>
+      <h1>DoctorDetails</h1>
+    </div>
+  )
+}
+
+export default DoctorDetails
--- frontEnd/src/routes/Routers.jsx
+++ frontEnd/src/routes/Routers.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import Home from "../pages/Home";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
+import Services from "../pages/Services";
+
+import { Routes, Route } from "react-router-dom";
+
 
 const Routers = () => {
   return (
-    <div>
-      <h1>Routers</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
   )
 }
 
