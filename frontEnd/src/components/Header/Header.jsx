ea4c4aa2b97a9dee7e308d9e3826032a3f1f5a44
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-05-08 15:26:38 +0530

    before fix code

--- frontEnd/src/components/Header/Header.jsx
+++ frontEnd/src/components/Header/Header.jsx
@@ -1,11 +1,109 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import logo from "../../assets/images/logo.png";
+import { NavLink } from "react-router-dom";
+import userImg from "../../assets/images/avatar-icon.png";
+import { BiMenu } from "react-icons/bi";
+
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/doctors",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
 
 const Header = () => {
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  )
-}
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
 
-export default Header
+  const handleStickyHeader = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky_header");
+      } else {
+        headerRef.current.classList.remove("sticky_header");
+      }
+    })
+  }
+
+  useEffect(()=>{
+    handleStickyHeader()
+
+    return ()=> window.removeEventListener('scroll', handleStickyHeader)
+  });
+  // could be wrong
+  const toggleMenu = () => menuRef.current.classList.toggle('show__Menu');
+
+  return (
+    <header className="header flex items-center" ref={headerRef}>
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/*==========logo============*/}
+          <div>
+            <img src={logo} alt="logo" />
+          </div>
+
+          {/*===========menu==========*/}
+          <div className="navigation" ref={muneRef} onClick={toggleMenu}>
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={Link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                        : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
+                    }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* ====nav right=========*/}
+          <div className="flex items-center gap-4">
+            <div className="hidden">
+              <Link to="/">
+                <figure className="w-[35px] h-[35px] rounded-full">
+                  <img src={userImg} className="w-full rounded-full" alt="dp" />
+                </figure>
+              </Link>
+            </div>
+            {/* =======login====== */}
+            <Link to="/login">
+              <button
+                className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center
+                justify-center rounded-[50px]"
+              >
+                Login
+              </button>
+            </Link>
+            {/* ======SideBar=======*/}
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -7,10 +7,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        primaryColor: "#141E46",
-        secondaryColor: "#BB2525",
-        tertiaryColor: "#FF6969",
-        quaternaryColor: "#FFF5E0",
+        primaryColor: "#0067FF",
+        yellowColor: "#FEB60D",
+        purpleColor: "#9771FF",
+        irisBlueColor: "#01B5C5",
+        headingColor: "#181A1E",
+        textColor: "#4E545F",
       },
 
       boxShadow: {
