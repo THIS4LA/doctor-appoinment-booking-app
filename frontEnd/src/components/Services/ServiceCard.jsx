a3413c6146e38836c53f328c892618bdc06bfbca
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-05-12 21:02:24 +0530

    Services completed

--- frontEnd/src/components/Services/ServiceCard.jsx
+++ frontEnd/src/components/Services/ServiceCard.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { BiArrowToRight } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
-const ServiceCard = () => {
+const ServiceCard = ({ item, index }) => {
+  const { name, desc, bgcolor, textColor } = item;
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="py-[30px] px-3 lg:px-5">
+      <h2 className="text-[26px] leading-9 text-headingColor font-[700]">
+        {name}
+      </h2>
+      <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+        {desc}
+      </p>
 
-export default ServiceCard
+      <div className="flex items-center justify-between mt-[30px]">
+        <Link
+          to="/doctors"
+          className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] 
+            flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+        >
+          <BiArrowToRight className="group-hover:text-white w-6 h-5" />
+        </Link>
+        <span
+          className="w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]"
+          style={{
+            backgroundColor: `${bgcolor}`,
+            color: `${textColor}`,
+            borderRadius: "6px 0 0 6px",
+          }}
+        >
+          {index + 1}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import About from './../components/About/About'
+import About from './../components/About/About';
+import ServiceList from '../components/Services/ServiceList';
 
 import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
@@ -7,6 +8,9 @@ import heroImg03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
+import featureImg from "../assets/images/feature-img.png";
+import videoIcon from "../assets/images/video-icon.png";
+import avatarIcon from "../assets/images/avatar-icon.png";
 import { Link } from "react-router-dom";
 
 import { BiArrowToRight } from 'react-icons/bi';
@@ -161,6 +165,100 @@ const Home = () => {
           </div>
         </section>
         <About />
+        {/* ============services=============== */}
+        <section>
+          <div className='container'>
+            <div className='xl:w-[400px] mx-auto'>
+              <h2 className='heading text-center'>
+                Our medical services
+              </h2>
+              <p className='text__para text-center'>
+                world-class care for everyone. Our health system offers
+                unmatched,expert health care.From the lab to the clinic.
+              </p>
+            </div>
+            <ServiceList/>
+          </div>
+        </section>
+        {/*=========Services section end============*/}
+        {/* ===========feature section========== */}
+        <section>
+          <div className='container'>
+            <div className='flex items-center justify-between flex-col lg:flex-row'>
+              {/* ========feature section content========== */}
+              <div className='xl:w-[670px]'>
+                <h2 className='heading'>
+                  Get virtual treatment <br /> anytime.
+                </h2>
+                <ul className='pl-4'>
+                  <li className='text__para'>
+                    1.Schedule the appointment directly.
+                  </li>
+                  <li className='text__para'>
+                    2.Search for your physician here, and contact their office.
+                  </li>
+                  <li className='text__para'>
+                    2.View our physicians who are accepting new patients,
+                     use the online scheduling tool to select an appointment time.
+                  </li>
+                </ul>
+                <Link to="/">
+                  <button className='btn'>Learn More</button>
+                </Link>
+              </div>
+              {/* ========feature section image========== */}
+              <div className='relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0'>
+                <img
+                  src={featureImg}
+                  alt='featureImg'
+                  className='w-3/4'
+                />
+                <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px]
+                md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]'>
+                  <div className='flex items-center justify-between'>
+                    <div className='flex items-center gap-[6px] lg:gap-3'>
+                      <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>
+                        Tue, 24
+                      </p>
+                      <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]'>
+                        10:00 AM
+                      </p>
+                    </div>
+                    <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded
+                    py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                      <img src={videoIcon} alt='videoIcon' />
+                    </span>
+                  </div>
+                  <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px]
+                  text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4
+                  rounded-full'>
+                    Consultation
+                  </div>
+                  <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
+                    <img src={avatarIcon} alt='avatarIcon' />
+                    <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor'>
+                      Kanchana Theyjan
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* ===========feature section end========== */}
+        {/* ===========our great doctors========== */}
+        <section>
+          <div className='container'>
+            <div className='xl:w-[470px] mx-auto'>
+              <h2 className='heading text-center'>Our great doctors</h2>
+              <p className='text__para text-center'>
+                world-class care for everyone. Our health system offers
+                unmatched,expert health care.From the lab to the clinic.
+              </p>
+            </div>
+          </div>
+        </section>
+        {/* ===========our great doctors end========== */}
       </>
   );
 };
