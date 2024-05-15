c31b16ab3d969b3c49c4f51a37291a00a505b197
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-05-15 20:29:53 +0530

    Doctor page inpage routing

--- frontEnd/src/pages/Doctors/DoctorDetails.jsx
+++ frontEnd/src/pages/Doctors/DoctorDetails.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import { useState } from "react";
+import doctorImg from "../../assets/images/doctor-img02.png";
+import starIcon from "../../assets/images/Star.png";
+import DoctorAbout from "./DoctorAbout";
+import DoctorFeedBack from "./DoctorFeedBack";
 
 const DoctorDetails = () => {
-  return (
-    <div>
-      <h1>DoctorDetails</h1>
-    </div>
-  )
-}
+  const [tab, setTab] = useState("about");
 
-export default DoctorDetails
+  return (
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        <div className="grid md:grid-cols-3 gap-[50px]">
+          <div className="md:col-span-2">
+            <div className="flex items-center gap-5">
+              <figure className="max-w-[200px] max-h-[200px]">
+                <img src={doctorImg} alt="doctorImg" className="w-full" />
+              </figure>
+
+              <div>
+                <span
+                  className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px]
+                leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded"
+                >
+                  Surgeon
+                </span>
+                <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
+                  Kanchana Theyjan
+                </h3>
+                <div className="flex items-center gap-[6px] ">
+                  <span
+                    className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px]
+                    lg:leading-7 font-semibold text-headingColor"
+                  >
+                    <img src={starIcon} alt="starIcon" />
+                    4.8
+                  </span>
+                  <span
+                    className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px]
+                    lg:leading-7 text-[400] font-semibold text-textColor"
+                  >
+                    (272)
+                  </span>
+                </div>
+                <p className="text__para text-[14px] leading-6 md:text-[15px] lg:max-w-[390px]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                  ex maiores laboriosam, mollitia iusto blanditiis in eveniet
+                </p>
+              </div>
+            </div>
+            {/* inpage tabs */}
+            <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
+              <button
+                onClick={() => setTab("about")}
+                className={`${
+                  tab === "about" && "border-b border-solid border-primaryColor"
+                } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                About
+              </button>
+              <button
+                onClick={() => setTab("feedback")}
+                className={`${
+                  tab === "feedback" && "border-b border-solid border-primaryColor"
+                } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                FeedBack
+              </button>
+            </div>
+            {/* inpage tabs */}
+                <div className="mt-[50px]">
+                  {
+                    tab === 'about' && <DoctorAbout />
+                  }
+                  {
+                    tab === 'feedback' && <DoctorFeedBack />
+                  }
+                </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DoctorDetails;
---
+++ frontEnd/src/pages/Doctors/DoctorFeedBack.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DoctorFeedBack = () => {
+  return (
+    <div>
+      feedBack
+    </div>
+  )
+}
+
+export default DoctorFeedBack
