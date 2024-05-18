35b21422545ee74a7539e3aad7d8f12dac8e5372
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-05-19 01:49:21 +0530

    doctor and user controller complete

--- backEnd/index.js
+++ backEnd/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoute from "./Routes/auth.js";
+import userRoute from "./Routes/user.js"
+import doctorRoute from "./Routes/doctor.js"
 
 dotenv.config();
 
@@ -35,6 +37,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use('/api/v1/auth', authRoute) //domain/api/v1/auth/register
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/doctors', doctorRoute);
 
 app.listen(port, () => {
   connectDB();
