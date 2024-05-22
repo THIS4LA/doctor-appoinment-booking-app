6c9972773e2879875362e9e9a81078f0a30f8193
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-05-22 06:19:28 +0530

    header user logged in display

--- frontEnd/src/components/Header/Header.jsx
+++ frontEnd/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useContext } from "react";
 import logo from "../../assets/images/logo.png";
 import { NavLink, Link } from "react-router-dom";
 import userImg from "../../assets/images/avatar-icon.png";
 import { BiMenu } from "react-icons/bi";
+import { authContext } from "../../context/AuthContext";
 
 const navLinks = [
   {
@@ -26,6 +27,7 @@ const navLinks = [
 const Header = () => {
   const headerRef = useRef(null);
   const menuRef = useRef(null);
+  const { user, role, token } = useContext(authContext);
 
   const handleStickyHeader = () => {
     window.addEventListener("scroll", () => {
@@ -79,22 +81,37 @@ const Header = () => {
 
           {/* ====nav right=========*/}
           <div className="flex items-center gap-4">
-            <div className="hidden">
-              <Link to="/">
-                <figure className="w-[35px] h-[35px] rounded-full">
-                  <img src={userImg} className="w-full rounded-full" alt="dp" />
-                </figure>
-              </Link>
-            </div>
-            {/* =======login====== */}
-            <Link to="/login">
-              <button
-                className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center
+            {token && user ? (
+              <div>
+                <Link
+                  to={`${
+                    role === "doctor"
+                      ? "/doctors/profile/me"
+                      : "/users/profile/me"
+                  }`}
+
+                  className="flex items-center gap-2"
+                >
+                  <h2 className="text-[16px] font-[500] text-headingColor">{user?.name}</h2>
+                  <figure className="w-[35px] h-[35px] rounded-full overflow-hidden">
+                    <img
+                      src={user?.photo}
+                      className="w-full rounded-full object-cover"
+                      alt="dp"
+                    />
+                  </figure>
+                </Link>
+              </div>
+            ) : (
+              <Link to="/login">
+                <button
+                  className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center
                 justify-center rounded-[50px]"
-              >
-                Login
-              </button>
-            </Link>
+                >
+                  Login
+                </button>
+              </Link>
+            )}
             {/* ======SideBar=======*/}
             <span className="md:hidden" onClick={toggleMenu}>
               <BiMenu className="w-6 h-6 cursor-pointer" />
--- frontEnd/src/context/AuthContext.jsx
+++ frontEnd/src/context/AuthContext.jsx
@@ -7,9 +7,9 @@ import {
 } from "react";
 
 const initialState = {
-  user: null,
-  role: null,
-  token: null
+  user: localStorage.getItem('user') != undefined ? JSON.parse(localStorage.getItem('user')) : null,
+  role: localStorage.getItem('role') || null,
+  token: localStorage.getItem('token') || null,
 };
 
 
@@ -46,6 +46,12 @@ const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(()=>{
+    localStorage.setItem("user", JSON.stringify(state.user));
+    localStorage.setItem("token", state.token);
+    localStorage.setItem("role", state.role);
+  }, [state]);
 
   return (
     <authContext.Provider
