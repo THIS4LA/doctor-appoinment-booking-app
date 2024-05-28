e6c50b5b2e86c41766b88d5fa39841f31822900d
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-05-28 06:47:35 +0530

    doctor details page

--- frontEnd/src/pages/Doctors/DoctorDetails.jsx
+++ frontEnd/src/pages/Doctors/DoctorDetails.jsx
@@ -1,21 +1,48 @@
 import { useState } from "react";
-import doctorImg from "../../assets/images/doctor-img02.png";
 import starIcon from "../../assets/images/Star.png";
 import DoctorAbout from "./DoctorAbout";
 import DoctorFeedBack from "./DoctorFeedBack";
 import SidePanel from "./SidePanel";
+import { BASE_URL } from "../../config";
+import useFetchData from "../../hooks/useFetchData";
+import Loading from "../../components/Loader/Loading";
+import Error from "../../components/Error/Error";
+import { useParams } from "react-router-dom";
 
 const DoctorDetails = () => {
   const [tab, setTab] = useState("about");
 
+  const {id} = useParams()
+
+  const { data: doctor, loading, error } = useFetchData(`${BASE_URL}/doctors/${id}`);
+
+  const {
+    name,
+    qualifications,
+    experience,
+    timeSlots,
+    reviews,
+    bio,
+    about,
+    averageRating,
+    totalRating,
+    specialization,
+    ticketPrice,
+    photo,
+  } = doctor || {} ;
+
   return (
     <section>
       <div className="max-w-[1170px] px-5 mx-auto">
-        <div className="grid md:grid-cols-3 gap-[50px]">
+
+      {loading && <Loading />}
+      {error && <Error />}
+
+        {!loading && !error && <div className="grid md:grid-cols-3 gap-[50px]">
           <div className="md:col-span-2">
             <div className="flex items-center gap-5">
-              <figure className="max-w-[200px] max-h-[200px]">
-                <img src={doctorImg} alt="doctorImg" className="w-full" />
+              <figure className="p-3 lg:p-5">
+                <img src={photo} alt="doctorImg" className="w-[300px] h-[300px] object-cover rounded" />
               </figure>
 
               <div>
@@ -23,10 +50,10 @@ const DoctorDetails = () => {
                   className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px]
                 leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded"
                 >
-                  Surgeon
+                  {specialization }
                 </span>
                 <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
-                  Kanchana Theyjan
+                  {name}
                 </h3>
                 <div className="flex items-center gap-[6px] ">
                   <span
@@ -34,18 +61,17 @@ const DoctorDetails = () => {
                     lg:leading-7 font-semibold text-headingColor"
                   >
                     <img src={starIcon} alt="starIcon" />
-                    4.8
+                    {averageRating}
                   </span>
                   <span
                     className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px]
                     lg:leading-7 text-[400] font-semibold text-textColor"
                   >
-                    (272)
+                    ({totalRating})
                   </span>
                 </div>
                 <p className="text__para text-[14px] leading-6 md:text-[15px] lg:max-w-[390px]">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-                  ex maiores laboriosam, mollitia iusto blanditiis in eveniet
+                  {bio}
                 </p>
               </div>
             </div>
@@ -78,7 +104,7 @@ const DoctorDetails = () => {
           <div>
             <SidePanel />
           </div>
-        </div>
+        </div>}
       </div>
     </section>
   );
